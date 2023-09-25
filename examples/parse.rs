f1c3ec47f6a2cfe26a6ed00a8cda766db0dc1c1e
Kornel <kornel@geekhood.net>
2023-09-25 20:10:15 +0100

    Add example

---
+++ examples/parse.rs
@@ -0,0 +1,14 @@
+//! If you want a complex example, see
+//! <https://gitlab.com/lib.rs/main/-/blob/6181e6a1db23f03586ff0d6006c42727c817b933/crate_git_checkout/src/crate_git_checkout.rs#L225-255>
+
+use std::path::PathBuf;
+use cargo_toml::Manifest;
+
+fn main() {
+    let path = std::env::args_os().nth(1).map(PathBuf::from)
+        .expect("Please specify path to Cargo.toml");
+
+    let manifest = Manifest::from_path(&path).unwrap();
+
+    eprintln!("{manifest:#?}");
+}

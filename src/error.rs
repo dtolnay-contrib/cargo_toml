537094c013b29f7e29ede17ead99004f86ea936b
Kornel <kornel@geekhood.net>
2025-03-08 21:11:21 +0000

    Smaller err

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,7 @@ use std::{fmt, io};
 #[non_exhaustive]
 pub enum Error {
     /// TOML parsing errors
-    Parse(toml::de::Error),
+    Parse(Box<toml::de::Error>),
     /// Filesystem access errors
     Io(io::Error),
     /// Manifest uses workspace inheritance, and the workspace failed to load
@@ -21,10 +21,10 @@ pub enum Error {
 
 impl StdErr for Error {
     fn source(&self) -> Option<&(dyn StdErr + 'static)> {
-        match *self {
-            Error::Parse(ref err) => Some(err),
-            Error::Io(ref err) => Some(err),
-            Error::Workspace(ref err) => Some(err),
+        match self {
+            Error::Parse(err) => Some(err),
+            Error::Io(err) => Some(err),
+            Error::Workspace(err) => Some(err),
             Error::Other(_) | Error::InheritedUnknownValue | Error::WorkspaceIntegrity(_) => None,
         }
     }
@@ -32,11 +32,11 @@ impl StdErr for Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Error::Parse(ref err) => err.fmt(f),
-            Error::Io(ref err) => err.fmt(f),
+        match self {
+            Error::Parse(err) => err.fmt(f),
+            Error::Io(err) => err.fmt(f),
             Error::Other(msg) => f.write_str(msg),
-            Error::WorkspaceIntegrity(ref s) => f.write_str(s),
+            Error::WorkspaceIntegrity(s) => f.write_str(s),
             Error::Workspace(_) => f.write_str("can't load root workspace"),
             Error::InheritedUnknownValue => f.write_str("value from workspace hasn't been set"),
         }
@@ -45,12 +45,12 @@ impl fmt::Display for Error {
 
 impl Clone for Error {
     fn clone(&self) -> Self {
-        match *self {
-            Error::Parse(ref err) => Error::Parse(err.clone()),
-            Error::Io(ref err) => Error::Io(io::Error::new(err.kind(), err.to_string())),
+        match self {
+            Error::Parse(err) => Error::Parse(err.clone()),
+            Error::Io(err) => Error::Io(io::Error::new(err.kind(), err.to_string())),
             Error::Other(msg) => Error::Other(msg),
-            Error::WorkspaceIntegrity(ref msg) => Error::WorkspaceIntegrity(msg.clone()),
-            Error::Workspace(ref e) => Error::Workspace(e.clone()),
+            Error::WorkspaceIntegrity(msg) => Error::WorkspaceIntegrity(msg.clone()),
+            Error::Workspace(e) => Error::Workspace(e.clone()),
             Error::InheritedUnknownValue => Error::InheritedUnknownValue,
         }
     }
@@ -58,7 +58,7 @@ impl Clone for Error {
 
 impl From<toml::de::Error> for Error {
     fn from(o: toml::de::Error) -> Self {
-        Error::Parse(o)
+        Error::Parse(Box::new(o))
     }
 }
 
